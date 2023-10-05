99cfa42ad8ce84a707dd9d581ea7e6891c8b77a8
Halal1122 <mdhealaluddin505@gmail.com>
2023-10-05 17:55:11 +0600

    right side added

--- src/Sheared/RightSide/RightSide.jsx
+++ src/Sheared/RightSide/RightSide.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 
 const RightSide = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(res=>res.json())
+        .then(data=>setCategories(data))
+    },[])
     return (
         <div>
-            <h2 className="text-2xl">Right Side</h2>
+            <h2 className="text-2xl m-2 font-semibold">All Categories</h2>
+            <h2 className="text-2xl font-semibold text-center p-2 border m-2 bg-slate-300 rounded-lg mt-3">National News</h2>
+            <div className="space-y-5 p-4">
+            {
+                categories.map(category => <NavLink 
+                    className="block ml-4 text-xl font-semibold text-slate-500" 
+                    key={category.id} to={`/category/${category.id}`}>
+                        {category.name}</NavLink>)
+            }
+            </div>
         </div>
     );
 };
